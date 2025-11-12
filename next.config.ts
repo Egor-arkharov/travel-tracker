ae13e786ae0a9bc8dfde6472f78bf543af678e37
EArkharov <egorarkharov@gmail.com>
2025-11-12 12:41:28 +0400

    update csp

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,15 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const csp = [
   "default-src 'self'",
   "base-uri 'self'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://www.googletagmanager.com https://www.google-analytics.com",
-  "connect-src 'self' https://*.googleapis.com https://*.gstatic.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebasestorage.googleapis.com https://*.vercel-insights.com",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://apis.google.com https://accounts.google.com https://www.googletagmanager.com https://www.google-analytics.com",
+  "connect-src 'self' https://*.googleapis.com https://oauth2.googleapis.com https://accounts.google.com https://*.gstatic.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebasestorage.googleapis.com https://*.vercel-insights.com",
   "img-src 'self' data: blob: https://*.gstatic.com https://*.googleapis.com https://lh3.googleusercontent.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' https://fonts.gstatic.com",
-  "frame-src https://www.google.com",
+  "frame-src https://www.google.com https://accounts.google.com",
   "frame-ancestors 'self'",
 ].join("; ");
+
 
 const securityHeaders = [
   { key: "Content-Security-Policy", value: csp },
@@ -30,7 +31,7 @@ const securityHeaders = [
 
 const config: NextConfig = {
   reactStrictMode: true,
-  
+
   async headers() {
     return [{ source: "/(.*)", headers: securityHeaders }];
   },
