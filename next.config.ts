2c42af9afac237c08cbc151c4967d7db05a3923c
EArkharov <egorarkharov@gmail.com>
2025-11-12 13:04:22 +0400

    update csp

--- next.config.ts
+++ next.config.ts
@@ -12,13 +12,15 @@ const csp = [
   "default-src 'self'",
   "base-uri 'self'",
   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://apis.google.com https://accounts.google.com https://www.googletagmanager.com https://www.google-analytics.com",
-  "connect-src 'self' https://*.googleapis.com https://oauth2.googleapis.com https://accounts.google.com https://*.gstatic.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebasestorage.googleapis.com https://*.vercel-insights.com",
+  "connect-src 'self' https://*.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://oauth2.googleapis.com https://accounts.google.com https://*.gstatic.com https://*.firebaseio.com https://firebasestorage.googleapis.com https://*.vercel-insights.com",
+  // Картинки/аватары/спрайты
   "img-src 'self' data: blob: https://*.gstatic.com https://*.googleapis.com https://lh3.googleusercontent.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' https://fonts.gstatic.com",
   "frame-src https://www.google.com https://accounts.google.com https://*.firebaseapp.com",
   "frame-ancestors 'self'",
 ].join("; ");
+
 
 
 const securityHeaders = [
